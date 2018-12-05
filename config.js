5059795794dfc8dc2545429f8295964b8919b3b7
Cameron Hatch <cameronlavigne92@gmail.com>
2018-12-05 14:05:38 -0700

    added dotenv to config

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 'use strict';
+
+require('dotenv').config();
+
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/buvie';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/buvie-test';
 exports.PORT = process.env.PORT || 8080;

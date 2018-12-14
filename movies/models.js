1f42625502e3b3aec1b681a9e731dece1ecbfc1e
Cameron Hatch <cameronlavigne92@gmail.com>
2018-12-14 09:35:41 -0700

    updated movie model to include omdb id

--- movies/models.js
+++ movies/models.js
@@ -1,22 +1,24 @@
-"use strict";
-const mongoose = require("mongoose");
+'use strict';
+const mongoose = require('mongoose');
 
 const MovieSchema = mongoose.Schema({
-	title: String,
-	genre: String,
-	poster: String,
-	users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
+  title: String,
+  genre: String,
+  poster: String,
+  users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  omdbId: String
 });
 
 MovieSchema.methods.serialize = function() {
-	return {
-		id: this._id || "",
-		title: this.title || "",
-		genre: this.genre || "",
-		poster: this.poster || "",
-	};
+  return {
+    id: this._id || '',
+    title: this.title || '',
+    genre: this.genre || '',
+    poster: this.poster || '',
+    omdbId: this.omdbId || ''
+  };
 };
 
-const Movie = mongoose.model("Movie", MovieSchema);
+const Movie = mongoose.model('Movie', MovieSchema);
 
 module.exports = { Movie };

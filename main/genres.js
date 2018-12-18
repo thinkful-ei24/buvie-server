1535bdd400d184a510358e72e5e22c15360c525e
Cameron Hatch <cameronlavigne92@gmail.com>
2018-12-18 09:54:39 -0700

    added error for more than 3 genres

--- main/genres.js
+++ main/genres.js
@@ -21,7 +21,9 @@ router.put('/:id', jsonParser, (req, res, next) => {
 
   if (genres) {
     if (genres.length > 3) {
-      genres = genres.slice(0, 3);
+      let err = new Error('Please only select 3 genres');
+      err.status = 400;
+      next(err);
     }
     if (!genres.length) {
       let err = new Error('Please select at least 1 genre');
